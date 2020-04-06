428c5e1bf3c6baa158e07dc8be1e854edd160095
stampid <vashstampid03@gmail.com>
2020-04-06 19:22:20 +0900

    Add services emailAuth

--- src/services/member.js
+++ src/services/member.js
@@ -2,7 +2,46 @@ import bcrypt from 'bcryptjs';
 import { sequelize } from '../../models/index';
 
 import * as memberComponenet from '../component/member';
+import * as authServcies from './auth';
+import * as redisServices from './redis';
+import { throwError } from '../router/decorator';
 
+/**
+ * @description 이메일 인증 문자 전송
+ * @param {String} email
+ * @param {*} redisClient
+ */
+export const sendEmail = async (email, redisClient) => {
+	try {
+		const secretWord = authServcies.generateSecret();
+
+		await authServcies.sendMail(email, secretWord);
+		await redisServices.setRedisKeyValue(email, secretWord, redisClient);
+
+		return null;
+	} catch (err) {
+		return throwError(err, 500);
+	}
+};
+
+/**
+ * @description 인증 이메일 문자 검증
+ * @param {String} email
+ * @param {String} secretWord
+ * @param {*} redisClient
+ */
+export const authEmail = async (email, secretWord, redisClient) => {
+	const response = { verify: await redisServices.getRedisKeyValue(email, secretWord, redisClient) };
+
+	await redisServices.deleteRedisKey(email, redisClient);
+	return response;
+};
+
+/**
+ * @description 계정 생성
+ * @param {Object} memberDataParam
+ * @param {Object} memberAttributeDataParam
+ */
 export const createMemberAccount = async (memberDataParam, memberAttributeDataParam) => {
 	const memberData = memberDataParam;
 	const memberAttributeData = memberAttributeDataParam;
@@ -25,5 +64,3 @@ export const createMemberAccount = async (memberDataParam, memberAttributeDataPa
 
 	return response;
 };
-
-export const etc = async () => {};
