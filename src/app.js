43bbbb9b9008aa6fa4fb46a59fdaba3cda50befa
stampid <vashstampid03@gmail.com>
2020-04-06 19:21:09 +0900

    Add app redis

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import csurf from 'csurf';
 import socketIo from 'socket.io';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import redis from 'redis';
 import router from './router/router';
 import socketController from './socket/socketController';
 
@@ -15,12 +16,22 @@ dotenv.config();
 const app = express();
 const { PORT } = process.env || 5000;
 
+const redisClient = redis.createClient({
+	host: process.env.REDIS_HOST,
+	port: process.env.REDIS_PORT,
+});
+
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use('/', (req, res, next) => {
+	req.redis_client = redisClient;
+	next();
+});
 
 app.use('/', router);
 
