525846a3ba7d9734dabd6001b2edbdd50a749c40
stampid <vashstampid03@gmail.com>
2020-04-06 19:22:12 +0900

    Add router emailAuth

--- src/router/member.js
+++ src/router/member.js
@@ -1,11 +1,31 @@
 import express from 'express';
-// import * as firstvalidarots from '../validators/first';
 import { doAsync } from './decorator';
 import * as memberServices from '../services/member';
 import routes from '../routes';
 
 const router = express.Router();
 
+/** @description 인증 이메일 전송 */
+router.post(
+	'/email',
+	doAsync(async (req, res) => {
+		const response = await memberServices.sendEmail(req.body.email, req.redis_client);
+
+		return res.json(response);
+	}),
+);
+
+/** @description 인증 이메일 문자 검증 */
+router.post(
+	'/email/verify',
+	doAsync(async (req, res) => {
+		const response = await memberServices.authEmail(req.body.email, req.body.secret_word, req.redis_client);
+
+		return res.json(response);
+	}),
+);
+
+/** @description site 회원 가입 */
 router.post(
 	routes.home,
 	doAsync(async (req, res) => {
@@ -13,7 +33,7 @@ router.post(
 			user_id: req.body.user_id,
 			password: req.body.password,
 			nickname: req.body.nickname,
-			join_site: req.body.join_site,
+			join_site: 'site',
 		};
 		const memberAttributeData = {
 			email: req.body.email,
