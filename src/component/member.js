c685999aaf1be287c10778cebbb1aa0085d24841
stampid <vashstampid03@gmail.com>
2020-04-06 19:22:32 +0900

    Fix componenet createMember

--- src/component/member.js
+++ src/component/member.js
@@ -1,10 +1,20 @@
 import { Member, MemberAttribute } from '../../models/index';
 
+/**
+ * @description 회원 정보 생성
+ * @param {Object} memberData
+ * @param {*} t
+ */
 export const createMember = async (memberData, t) => {
 	const response = await Member.create(memberData, { transaction: t });
 	return response;
 };
 
+/**
+ * @description 회원 기본 정보 생성
+ * @param {Object} memberAttributeData
+ * @param {*} t
+ */
 export const createMemberAttribute = async (memberAttributeData, t) => {
 	const response = await MemberAttribute.create(memberAttributeData, { transaction: t });
 	return response;
