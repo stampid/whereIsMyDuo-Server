da2431e1790fe5f620c900b1d45f1b9ab8157176
stampid <vashstampid03@gmail.com>
2020-04-22 19:55:38 +0900

    Add route auth

--- src/router/router.js
+++ src/router/router.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import routes from '../routes';
 import member from './member';
+import jwt from './jwt';
 
 const router = express.Router();
 
 router.use(routes.member, member);
+
+router.use(routes.auth, jwt);
 
 export default router;
