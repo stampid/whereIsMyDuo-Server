010fdd34cb27147765598e01f2ff886e129f6431
stampid <vashstampid03@gmail.com>
2020-04-22 20:13:12 +0900

    Fix router decorator

--- src/router/decorator.js
+++ src/router/decorator.js
@@ -1,14 +1,7 @@
-export const doAsync = (fn) => async (req, res, next) => {
-	try {
-		await fn(req, res, next);
-	} catch (err) {
-		next(err);
-	}
-};
+export const doAsync = (fn) => (req, res, next) => fn(req, res, next).catch(next);
 
 export const throwError = (message, stautsCode) => {
 	const err = new Error(message);
 	err.status = stautsCode;
-
 	throw err;
 };
